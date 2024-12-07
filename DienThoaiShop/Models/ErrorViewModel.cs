ba1ee2aa5e99efcd06d0cf4b81e7aeb7e9c8d7d2
Phan Thanh Tinh <phanthanhtinh28012003@gmail.com>
2024-12-07 19:03:30 +0700

    Add project files.

---
+++ DienThoaiShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DienThoaiShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
