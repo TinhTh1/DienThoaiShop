a700e0e6e0b3043edd7a4e5d1492e078dd12d2cc
Phan Thanh Tinh <phanthanhtinh28012003@gmail.com>
2024-12-07 19:21:48 +0700

    Xong add-migration

--- DienThoaiShop/Program.cs
+++ DienThoaiShop/Program.cs
@@ -1,8 +1,12 @@
+using DTShop.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<DTShopDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DTShopConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
